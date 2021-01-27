41780edb285426e013c768798b655321d5205f47
joka043 <jonas.kasnitz@gmail.com>
2021-01-27 10:29:57 +0100

    garage positions

--- MenschAergereDichNicht/PositionHome.cs
+++ MenschAergereDichNicht/PositionHome.cs
@@ -15,25 +15,25 @@ namespace MenschAergereDichNicht
     }
     class PositionHome
     {
-        Pos[] positions_blue = new Pos[4];
-        Pos[] positions_red = new Pos[4];
-        Pos[] positions_yellow = new Pos[4];
-        Pos[] positions_green = new Pos[4];
+        Pos[] positions_home_blue = new Pos[4];
+        Pos[] positions_home_red = new Pos[4];
+        Pos[] positions_home_yellow = new Pos[4];
+        Pos[] positions_home_green = new Pos[4];
 
         Pos GetCoord(int pos, int col)
         {
             switch (col)
             {
                 case 0:
-                    return positions_blue[pos];
-                case 1:
-                    return positions_red[pos];
-                case 2:
-                    return positions_yellow[pos];
-                case 3:
-                    return positions_green[pos];
-                default:
-                    return positions_green[0];
+                    return positions_home_blue[pos];
+                case 1:             
+                    return positions_home_red[pos];
+                case 2:             
+                    return positions_home_yellow[pos];
+                case 3:             
+                    return positions_home_green[pos];
+                default:            
+                    return positions_home_green[0];
             }
 
         }
@@ -42,25 +42,25 @@ namespace MenschAergereDichNicht
 
         void fillHomePositions()
         {
-            positions_blue[0] = new Pos(PosConst.xPos_9, PosConst.yPos_0);
-            positions_blue[1] = new Pos(PosConst.xPos_10, PosConst.yPos_0);
-            positions_blue[2] = new Pos(PosConst.xPos_10, PosConst.yPos_1);
-            positions_blue[3] = new Pos(PosConst.xPos_9, PosConst.yPos_1);
+            positions_home_blue[0] = new Pos(PosConst.xPos_9, PosConst.yPos_0);
+            positions_home_blue[1] = new Pos(PosConst.xPos_10, PosConst.yPos_0);
+            positions_home_blue[2] = new Pos(PosConst.xPos_10, PosConst.yPos_1);
+            positions_home_blue[3] = new Pos(PosConst.xPos_9, PosConst.yPos_1);
 
-            positions_red[0] = new Pos(PosConst.xPos_9, PosConst.yPos_9);
-            positions_red[1] = new Pos(PosConst.xPos_10, PosConst.yPos_9);
-            positions_red[2] = new Pos(PosConst.xPos_10, PosConst.yPos_10);
-            positions_red[3] = new Pos(PosConst.xPos_9, PosConst.yPos_10);
+            positions_home_red[0] = new Pos(PosConst.xPos_9, PosConst.yPos_9);
+            positions_home_red[1] = new Pos(PosConst.xPos_10, PosConst.yPos_9);
+            positions_home_red[2] = new Pos(PosConst.xPos_10, PosConst.yPos_10);
+            positions_home_red[3] = new Pos(PosConst.xPos_9, PosConst.yPos_10);
 
-            positions_yellow[0] = new Pos(PosConst.xPos_0, PosConst.yPos_9);
-            positions_yellow[1] = new Pos(PosConst.xPos_1, PosConst.yPos_9);
-            positions_yellow[2] = new Pos(PosConst.xPos_1, PosConst.yPos_10);
-            positions_yellow[3] = new Pos(PosConst.xPos_0, PosConst.yPos_10);
+            positions_home_yellow[0] = new Pos(PosConst.xPos_0, PosConst.yPos_9);
+            positions_home_yellow[1] = new Pos(PosConst.xPos_1, PosConst.yPos_9);
+            positions_home_yellow[2] = new Pos(PosConst.xPos_1, PosConst.yPos_10);
+            positions_home_yellow[3] = new Pos(PosConst.xPos_0, PosConst.yPos_10);
 
-            positions_green[0] = new Pos(PosConst.xPos_0, PosConst.yPos_0);
-            positions_green[0] = new Pos(PosConst.xPos_1, PosConst.yPos_0);
-            positions_green[0] = new Pos(PosConst.xPos_1, PosConst.yPos_1);
-            positions_green[0] = new Pos(PosConst.xPos_0, PosConst.yPos_1);
+            positions_home_green[0] = new Pos(PosConst.xPos_0, PosConst.yPos_0);
+            positions_home_green[1] = new Pos(PosConst.xPos_1, PosConst.yPos_0);
+            positions_home_green[2] = new Pos(PosConst.xPos_1, PosConst.yPos_1);
+            positions_home_green[3] = new Pos(PosConst.xPos_0, PosConst.yPos_1);
 
         }
     }
